c1ee43bc7a27eca9ddf1491581fb2cd64dbe389f
varshitha5111 <varshithabkalmath@gmail.com>
2024-12-15 05:54:05 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
     try{
 
         $host="localhost";
-        $dbname="jobportal";
+        $dbname="";
         $user="root";
         $pass="";
         $conn=new PDO("mysql:host=$host;dbname=$dbname","$user","$pass");
